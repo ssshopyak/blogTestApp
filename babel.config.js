d7f7f2b2c557cdb9a203b941c14d56ad0e659fb2
Ostap Shopyak <90937268+ssshopyak@users.noreply.github.com>
2025-05-07 22:02:46 +0300

    features(basic architecture)

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,34 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@utils': './src/utils',
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@constants': './src/constants',
+          '@navigations': './src/navigations',
+          '@screens': './src/screens',
+          '@styles': './src/styles',
+          '@stores': './src/stores',
+          '@models': './src/models',
+        },
+      },
+    ],
+  ],
 };
